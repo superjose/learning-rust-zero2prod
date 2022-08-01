0bac0657b7520305360df9b08eac86ec3d6d01c8
superjose <superjose_49@hotmail.com>
2022-08-01 07:40:26 -0400

    feature(.env): added .env functionality

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,24 @@
+use env_logger::Env;
 use sqlx::PgPool;
 use std::net::TcpListener;
 
+use dotenv;
 use zero2prod::configuration::get_configuration;
 use zero2prod::startup::run;
 
 const BASE_URL: &str = "127.0.0.1";
 
 /**
- * Everything was moved to lib.rs to prevent clashes
- */
+Everything was moved to lib.rs to prevent clashes
+*/
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    dotenv::dotenv().ok();
+    // `init` call `set_logger`, so this is all we need to do.
+    //  We are also falling back to printing all the logs at info-level
+    //  or above if the RUST_LOG environment variable has not been set.
+    env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
+
     // Bubble up the io::Error if we failed to bind the address
     // Otherwise call .await on our Server
     let (listener, port) = get_listener();
@@ -18,7 +26,7 @@ async fn main() -> std::io::Result<()> {
     let connection = PgPool::connect(&configuration.database.connection_string())
         .await
         .expect("Failed to connect to Postgres.");
-    print!("Listening on {}:{}", BASE_URL, port);
+    println!("Listening on {}:{}", BASE_URL, port);
 
     run(listener, connection)?
         .await
